4a738e5bcae95f42fef0937ce70df13802fab1a5
Jayganesh Gupta <jayganeshgupta0@gmail.com>
2025-05-26 14:34:50 +0530

    route fixes

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import "./index.scss";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
